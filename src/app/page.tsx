6654ca386dca7fce22e07fda8ea39cc8bd9e8e79
Yuji Nakamura <ny5117@icloud.com>
2025-07-20 10:40:03 +0800

    stylling selector component and display it in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import LanguageSelector from "@/components/LanguageSelector";
+import { LANGUAGE_OPTIONS, LanguageCode } from "@/constants";
+import { ArrowRightLeft } from "lucide-react";
+import { useState } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const [fromLanguage, setFromLanguage] = useState<LanguageCode>("en");
+  const [toLanguage, setToLanguage] = useState<LanguageCode>("ja");
+
+  return (
+    <div className="w-full my-6">
+      <div className="flex justify-center gap-10 px-4">
+        <LanguageSelector
+          value={fromLanguage}
+          onChange={setFromLanguage}
+          options={LANGUAGE_OPTIONS}
+        />
+        <button>
+          <ArrowRightLeft className="w-4 h-4" />
+        </button>
+        <LanguageSelector
+          value={toLanguage}
+          onChange={setToLanguage}
+          options={LANGUAGE_OPTIONS}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,10 +1,10 @@
-import { LanguageOption } from "@/constants";
+import { LanguageCode, LanguageOption } from "@/constants";
 import React from "react";
 
 interface LanguageSelectorProps {
   value: string;
-  onChange: (value: string) => void;
-  options: LanguageOption[];
+  onChange: (value: LanguageCode) => void;
+  options: readonly LanguageOption[];
 }
 
 const LanguageSelector = ({
@@ -13,8 +13,12 @@ const LanguageSelector = ({
   options,
 }: LanguageSelectorProps) => {
   return (
-    <div>
-      <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <div className="w-full border border-gray-500 rounded-md bg-white px-2 py-1">
+      <select
+        value={value}
+        onChange={(e) => onChange(e.target.value as LanguageCode)}
+        className="w-full"
+      >
         {options.map((opt) => (
           <option key={opt.value} value={opt.value}>
             {opt.label}
